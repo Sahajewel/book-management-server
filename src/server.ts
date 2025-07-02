dee13efe23d9fc3d29effa272b5090841feefa0f
Saha-Jewel <jewelsaha072@gmail.com>
2025-07-02 18:42:03 +0900

    setup completed

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { Server } from "http";
+import app from "./app";
+let server: Server;
+const port = process.env.PORT || 5000;
+
+async function main(){
+    const mongoUser = process.env.USER;
+    const mongoPassword = process.env.PASSWORD;
+    if(!mongoUser || !mongoPassword){
+        throw new Error("Mongodb credentials are missing in .env file!")
+    }
+    try{
+        await mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.r7awt.mongodb.net/booksDB?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log("Connected to mongoose");
+        server = app.listen(port,()=>{
+            console.log(`App is running is on port: ${port}`)
+        })
+    }catch(error){
+        console.log(error)
+    }
+};
+main();
